da1506f9ec628ee5f5b213fbb88530b6134861e7
amolnagose12 <amolnagose12@gmail.com>
2021-12-26 02:42:05 +0530

    demonstrating various http methods

---
+++ src/test/java/com/bridgelabz/addressbook/AddressBookAppDevelopment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppDevelopment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
