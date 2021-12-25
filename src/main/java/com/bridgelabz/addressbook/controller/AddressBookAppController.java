b6235ed28a8bd84ef37c7a0f350cc4d6e144d544
amolnagose12 <amolnagose12@gmail.com>
2021-12-26 02:24:21 +0530

    demonstrate varios HTTP methods

---
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookAppController.java
@@ -0,0 +1,81 @@
+package com.bridgelabz.addressbook.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.addressbook.dto.AddressBookDTO;
+import com.bridgelabz.addressbook.dto.ResponseDTO;
+import com.bridgelabz.addressbook.model.AddressBookData;
+
+@RestController
+@RequestMapping("/addressbookapp")
+public class AddressBookAppController {
+	private static final AddressBookDTO addressBookDTO = null;
+	
+	/*RequestMapping:use to pass the URL.
+	 * return:message get call successful.
+	 */
+	@RequestMapping(value = { "/get" })
+	public ResponseEntity<ResponseDTO> getAddressBookData() {
+		AddressBookData addressbookData = null;
+		addressbookData = new AddressBookData(151, new AddressBookDTO("gulbarga", "karnatka"));
+		ResponseDTO respDTO = new ResponseDTO("Get call Successful:", addressbookData);
+		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+	}
+	
+	/*@GetMapping:passed URL with pinCod
+	 * return:response which takes the Code and return city state.
+	 */
+	
+	@GetMapping("/get/{pinCode}")
+	public ResponseEntity<ResponseDTO> getAddressBookData(@PathVariable("pinCode") int pinCode) {
+		AddressBookData addressbookData = null;
+		addressbookData = new AddressBookData(pinCode, new AddressBookDTO("mumbai", "Maharashtra"));
+		ResponseDTO respDTO = new ResponseDTO("Get call for pinCode Successful:", addressbookData);
+		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+	}
+	
+	/*PostMapping:Use to pass the url.
+	 * return:create the data by taking JSON file.
+	 */
+	
+	@PostMapping("/create")
+	public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressbookDTO) {
+		AddressBookData addressbookData = null;
+		addressbookData = new AddressBookData(1, addressbookDTO);
+		ResponseDTO respDTO = new ResponseDTO("Create AddressBook Data:", addressbookData);
+		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+	}
+	
+	/*put method is use to update the data.
+	 * by passing pinCode.
+	 */
+	
+	@PutMapping("/update/{pinCode}")
+	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("pinCode") int pinCode,
+			@RequestBody AddressBookDTO empPayrollDTO) {
+		AddressBookData addressbookData = null;
+		addressbookData = new AddressBookData(pinCode, addressBookDTO);
+		ResponseDTO respDTO = new ResponseDTO("Update AddressBook Successful:", addressbookData);
+		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+
+	}
+	
+	/*DeleteMapping: use to delete the data with pin code
+	 * return:the message that the data got deleted by using the pinCode.
+	 */
+	
+	@DeleteMapping("/delete/{pinCode}")
+	public ResponseEntity<ResponseDTO> deleteAddressBookData(@PathVariable("pinCode") int pinCode) {
+		ResponseDTO respDTO = new ResponseDTO("Deleted Successful,Deleted Code:", pinCode);
+		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.addressbook.dto;
+
+public class AddressBookDTO {
+	
+	public String state;
+	public String city;
+
+	public AddressBookDTO(String state, String city) {
+		super();
+		
+		this.state = state;
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBookDTO [state=" + state + ", city=" + city + "]";
+	}
+
+}
