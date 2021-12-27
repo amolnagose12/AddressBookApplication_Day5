a0f95c39b93bb0eade35fc3aa5a9d687fe2fc23e
amolnagose12 <amolnagose12@gmail.com>
2021-12-28 02:53:40 +0530

    DataBase setting as Environment variable

--- src/main/java/com/bridgelabz/addressbook/AddressBookAppDevelopmentApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookAppDevelopmentApplication.java
@@ -12,7 +12,8 @@ public class AddressBookAppDevelopmentApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookAppDevelopmentApplication.class, args);
-		log.info("AddressBook App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} Environment",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
-
 }
